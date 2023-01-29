ced73ec6ece35c471c20930bc292ef4995f13550
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-01-29 19:17:43 +0000

    Agregue el archivo ejercicio2.py

---
+++ ejercicios/ejercicio2.py
@@ -0,0 +1,7 @@
+"""
+Imagine you need to create a program for a shop that outputs the price of an item.
+The given program is designed to display Price: 87$
+But it’s incomplete.
+"""
+precio = "87$"  #  variable para almacenar el precio de un articulo
+print("Price:", precio)  #  concatena cadenas de caracteres, una variable y la imprime
