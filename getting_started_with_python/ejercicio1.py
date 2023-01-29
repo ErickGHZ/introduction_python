64513f9137b15c0c83f86df7c86a307775b8c3d8
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-01-29 19:39:34 +0000

    Agregue el archivo ejercicio1.py además de cambiar el nombre de otros archivos

--- getting_started_with_python/ejercicio1.py
+++ getting_started_with_python/ejercicio1.py
@@ -1,9 +1,12 @@
 """
-Ever wonder how many seconds are there in a day?
-Let's calculate it! 
+You need to calculate the points earned by a soccer team. 
+The team won 18 games and ended 7 games as a draw.  
+A win brings 3 points, while a draw brings 1.
 """
-horas_en_un_dia = 24  #  variable para almacenar las horas que hay en un dia
-minutos_en_una_hora = 60  #  variable para almacenar los minutos que hay en una hora
-segundos_en_un_minuto = 60  #  variable para almacenar los segundos que hay en un minuto
-segundos_en_un_dia = horas_en_un_dia * minutos_en_una_hora * segundos_en_un_minuto  #  variable para almacenar los segundos que hay en un dia mediante la multiplicacion de las otras variables
-print("Los segundos en un dia son",segundos_en_un_dia)  #  concatena cadenas de caracteres, una variable y la imprime
+puntos_por_ganar=3
+puntos_por_empatar=1
+juegos_gananados = 18
+juegos_empatados = 7
+puntos_ganados_por_ganar = juegos_gananados * puntos_por_ganar
+puntos_ganados_por_empatar = juegos_empatados * puntos_por_empatar
+print("Los puntos totales ganados del team son:",puntos_ganados_por_ganar + puntos_ganados_por_empatar)
