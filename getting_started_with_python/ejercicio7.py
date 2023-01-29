5e3c1fb6d55b59ed9bbbc7f3b179b1b23743d2be
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-01-29 19:59:11 +0000

    Agregue el archivo ejercicio7.py

---
+++ getting_started_with_python/ejercicio7.py
@@ -0,0 +1,5 @@
+"""
+Change the code so it outputs the value multiplied by 5.
+"""
+num = 4  #  variable para almacenar el valor de un numero
+print(num*5)  #  concatena cadenas de caracteres, y multiplica esa cadena por 5 y la imprime
