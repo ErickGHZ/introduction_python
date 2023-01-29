ae73a2448c7c0c8a63b72d79bbed9d7343159800
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-01-29 20:27:34 +0000

    agregue el archivo ejercicio9.py

--- getting_started_with_python/ejercicio1.py
+++ getting_started_with_python/ejercicio1.py
@@ -5,10 +5,10 @@ You need to calculate the points earned by a soccer team.
 The team won 18 games and ended 7 games as a draw.  
 A win brings 3 points, while a draw brings 1.
 """
-puntos_por_ganar=3
-puntos_por_empatar=1
-juegos_gananados = 18
-juegos_empatados = 7
-puntos_ganados_por_ganar = juegos_gananados * puntos_por_ganar
-puntos_ganados_por_empatar = juegos_empatados * puntos_por_empatar
-print("Los puntos totales ganados del team son:",puntos_ganados_por_ganar + puntos_ganados_por_empatar)
+puntos_por_ganar=3  #  variable para almacenar el valor de un numero
+puntos_por_empatar=1  #  variable para almacenar el valor de un numero
+juegos_gananados = 18  #  variable para almacenar el valor de un numero
+juegos_empatados = 7  #  variable para almacenar el valor de un numero
+puntos_ganados_por_ganar = juegos_gananados * puntos_por_ganar  #  variable para almacenar la suma de variables
+puntos_ganados_por_empatar = juegos_empatados * puntos_por_empatar  #  variable para almacenar la suma de variables
+print("Los puntos totales ganados del team son:",puntos_ganados_por_ganar + puntos_ganados_por_empatar)  #  concatena cadenas de caracteres, dos variables y las imprime
---
+++ getting_started_with_python/ejercicio9.py
@@ -0,0 +1,11 @@
+"""
+Flight Time
+
+You are at the airport and you need to calculate the flight time of an upcoming trip. You are flying from LA to Sydney, covering a distance of 7425 miles, and the plane flies at an average speed of 550 miles an hour.
+
+Task:
+Calculate and output the total flight time in hours.
+"""
+recorido = 7425  #  variable para almacenar el valor de un numero
+velocidad = 550  #  variable para almacenar el valor de un numero
+print(recorido/velocidad)   #  concatena cadenas de caracteres en este caso 2 variables
