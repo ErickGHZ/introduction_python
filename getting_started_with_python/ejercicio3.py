9470bd1dcb20cdfa0c800a373acb1a3d174d0482
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-01-29 19:24:06 +0000

    Agregue el archivo ejercicio3.py

---
+++ getting_started_with_python/ejercicio3.py
@@ -0,0 +1,8 @@
+"""
+The given code has been designed to display the user’s nickname and score in a video game. It should output:
+Nickname: Master
+Score: 99
+But there is something wrong. Can you fix it?
+"""
+print("Nickname:", "Master")  #  concatena cadenas de caracteres y la imprime
+print("Score:", 99)  #  concatena cadenas de caracteres, un int y lo imprime
