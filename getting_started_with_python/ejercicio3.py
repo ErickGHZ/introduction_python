e56c2f41e64e4d2da5e3823a7e91b4df2166ef9c
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-01-29 19:45:46 +0000

    Agregue el archivo ejercicio3.py

---
+++ getting_started_with_python/ejercicio3.py
@@ -0,0 +1,5 @@
+"""
+The given program is designed to output the text: Knowledge is power
+But the last word is missing. Can you fix it?
+"""
+print("Knowledge " + "is "+ "power ")  #  concatena cadenas de caracteres, las une y las imprime
