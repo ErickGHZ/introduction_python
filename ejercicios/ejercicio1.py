21da8baa0e19c59865f4beb6c6c5a34aaabff111
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-01-29 19:11:08 +0000

    Termine el ejercicio1.py ademas de haber agragado la carpeta ejercicios el archivo README.ms

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -6,4 +6,4 @@ horas_en_un_dia = 24  #  variable para almacenar las horas que hay en un dia
 minutos_en_una_hora = 60  #  variable para almacenar los minutos que hay en una hora
 segundos_en_un_minuto = 60  #  variable para almacenar los segundos que hay en un minuto
 segundos_en_un_dia = horas_en_un_dia * minutos_en_una_hora * segundos_en_un_minuto  #  variable para almacenar los segundos que hay en un dia mediante la multiplicacion de las otras variables
-print("Los segundos en dia son",segundos_en_un_dia)  #  concatena cadenas de caracteres, una variable y la imprime
+print("Los segundos en un dia son",segundos_en_un_dia)  #  concatena cadenas de caracteres, una variable y la imprime
