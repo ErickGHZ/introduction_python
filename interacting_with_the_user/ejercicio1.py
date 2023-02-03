c7a3956e909e3e2be60f0d2d0d45af5fe056c3a8
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-02-03 04:19:28 +0000

    Agregue la carpeta interacting_with_the_user y en ella agregue los archivos ejercicio1.py y ejercicio2.py

---
+++ interacting_with_the_user/ejercicio1.py
@@ -0,0 +1,17 @@
+"""
+Input
+
+Imagine you’re working on a new contact management system. The program is required to ask the user for name and age inputs.
+The following output needs to be produced: “name is age years old”, where name and age are variables.
+There’s something wrong with the given code, can you fix it?
+Complete the code so name and age can be provided by the user.
+
+Sample Input:
+Anna 18
+
+Sample Output:
+Anna is 18 years old
+"""
+name = input()  #  guarda la entrada que realiza el usuario en una variable
+age = input()  #  guarda la entrada que realiza el usuario en una variable
+print(name + " is " + age + " years old")  #  imprime una cadena de texto y una varibales
---
+++ interacting_with_the_user/ejercicio2.py
@@ -0,0 +1,20 @@
+"""
+Country Cards
+You need to make a program to create country cards in which the names of the country and the capital are displayed.
+
+Task
+Complete the code to take the country and the capital as input and result in the output you are given in the example below 👇.
+
+Input Example
+Portugal   
+Lisbon
+
+Output Example
+Country: Portugal
+Capital: Lisbon
+"""
+
+country = input()
+capital = input()
+print("Country:", country)
+print("Capital:", capital)
