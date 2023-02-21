bb3b2c77ca33f0b030fecb2f3e584fdbbfbf76ab
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-02-20 22:37:09 -0600

    Update programa0.py
    agregue el archivo programa0.py

--- list/programa0.py
+++ list/programa0.py
@@ -5,3 +5,7 @@ You are working on a queue management program.
 The queue is represented by a list. 
 Write a program to take an input, add it to the end of the queue, and output the resulting list.
 """
+queue = ['John', 'Amy', 'Bob', 'Adam']  #  crea una list
+x= input()   #  guarda una entrada en una variable
+queue.insert(x)  #  inserta en la list la varibale
+print(queue)  #  impreme las list
