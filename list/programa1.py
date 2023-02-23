69579e96be07d62e1672ba6c1ca2d5cbde20a851
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-02-22 23:47:31 -0600

    Cree programa.py

---
+++ list/programa1.py
@@ -0,0 +1,11 @@
+"""
+List Functions
+
+You are working on a queue management program.
+The queue is represented by a list. 
+Write a program to take an input, add it to the end of the queue, and output the resulting list.
+"""
+queue = ['John', 'Amy', 'Bob', 'Adam']  #  crea una lista
+x= input()  #  guarda una entrada de texto en una varianble
+queue.insert(x)  # inserta la en la lista la variable x
+print(queue)  #  imprime la lista
